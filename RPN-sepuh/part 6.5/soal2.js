227441731d34b3c04295a0f9c290e804e22307fe
Razeer <142573682+razer27@users.noreply.github.com>
2023-10-22 23:36:03 +0700

    Add files via upload

---
+++ RPN-sepuh/part 6.5/soal2.js
@@ -0,0 +1,16 @@
+// 2. Melakukan Looping Menggunakan For
+// Problem
+// Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax for.
+// Untuk membuat tantangan ini lebih menarik, kamu juga diminta untuk membuat suatu looping yang menghitung maju dan
+// menghitung mundur. Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
+//tampilkan output yang menunjukan kalau looping itu maju pada looping pertama dan mundur pada looping kedua
+
+console.log("looping pertama");
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+console.log("looping kedua");
+for (let j = 10; j >= 1; j--) {
+  console.log(j);
+}
