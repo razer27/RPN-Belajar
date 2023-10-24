58a637b44e1ae946c5b1c357ef09b0ce3346827e
Razeer <142573682+razer27@users.noreply.github.com>
2023-10-24 14:41:41 +0700

    Add files via upload

---
+++ RPN-sepuh/part 7.5/soal2.js
@@ -0,0 +1,18 @@
+// Tugas 2
+// Buatlah sebuah fungsi bernama calculateMultiply(), yang mengembalikan nilai berupa hasil kali dari
+// dua parameter yang dikirim.
+
+//bikin fungsinya disini
+
+let num1 = 1;
+let num2 = 2;
+function calculateMultiply() {
+  return num1 * num2;
+}
+
+let hasilPerkalian = calculateMultiply(num1, num2);
+console.log(hasilPerkalian);
+
+// ------------------------------------
+console.log("");
+// -----------------------------------
